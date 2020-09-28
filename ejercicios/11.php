fbe4359891bc890fd4bbe3451ae354788346cc38
alumno <adriansalozgz@gmail.com>
2020-09-28 18:17:25 +0100

    ejercicio11

---
+++ ejercicios/11.php
@@ -0,0 +1,21 @@
+
+</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Dime los siguientes datos del libro</h1>
+    <form method="post" action="11action.php">
+        <label>Título<input type="text" value="" name="titulo"><br>
+        <label>Autor</label><input type="text" value="" name="autor"><br>
+        <label>Editorial</label><input type="text" value="" name="editorial"><br>
+        <label>Páginas</label><input type="text" value="" name="paginas"><br>
+        <input type="submit" value="enviar">
+    </form>
+</body>
+</html>
+   
