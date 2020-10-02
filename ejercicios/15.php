b67a5bb80247edecd4698eab412a97355e1693b5
alumno <adriansalozgz@gmail.com>
2020-10-02 14:21:04 +0100

    ejercicio15

---
+++ ejercicios/15.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php if (!empty($_GET)) {?>      
+        <h1>Hola <?php foreach($_GET['nombres'] as $nombre){
+            echo "<ul><li>".$nombre."</li></ul>";
+        } ?></h1>
+        <?php }else {?>
+        <form action="" method="get" >  
+          <label>Nombre<input type="text" name="nombres[]" required >
+            <br>
+          <label>Nombre<input type="text" name="nombres[]" required >
+          <br>
+          <label>Nombre<input type="text" name="nombres[]" required >
+          <br>
+          <label>Enviar <input type="submit">
+        </form>
+        <?php } ?>
+
+</body>
+</html>
