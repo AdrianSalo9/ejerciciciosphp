35332a68c52ce8e397553d9486e45d903e21ec06
Adrian Salo <adriansalozgz@gmail.com>
2020-10-05 18:12:54 +0100

    arreglos

--- ejercicios/17/views/index.php
+++ ejercicios/17/views/index.php
@@ -1,8 +1,8 @@
 <header>
   <ul>
-    <li><a href="/ejercicios/POO?method=fibonacci">Fibonacci</a></li>
-    <li><a href="/ejercicios/POO?method=listaPares">Pares</a></li>
-    <li><a href="/ejercicios/POO?method=potencias">Potencias</a></li>
-    <li><a href="/ejercicios/POO?method=primos">Primos</a></li>
+    <li><a href="/ejercicios/17?method=fibonacci">Fibonacci</a></li>
+    <li><a href="/ejercicios/17?method=listaPares">Pares</a></li>
+    <li><a href="/ejercicios/17?method=potencias">Potencias</a></li>
+    <li><a href="/ejercicios/17?method=primos">Primos</a></li>
   </ul>
 </header>
