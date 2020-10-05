168d2c6d7d24beb67159ccf200ca67034264f93d
Adrian Salo <adriansalozgz@gmail.com>
2020-10-05 16:18:22 +0100

    ejercicio16

--- ejercicios/16.php
+++ ejercicios/16.php
@@ -6,6 +6,19 @@
     <title>Document</title>
 </head>
 <body>
-    
+<h2>Inserta tu nombre:</h2>
+  <form action="">
+    <input type="text" name="nombres[]">
+    <input type="submit" value="Nuevo">
+    <input type="hidden" name="repetido">
+    <hr>
+    <?php
+    if (isset($_GET['nombres'])) {
+      foreach($_GET['nombres'] as $nombre) {
+        echo '<ul><li><input type="text" name="nombres[]" value="' .$nombre . '"></li></ul>';
+      }
+    }
+    ?>
+  </form>
 </body>
 </html>
