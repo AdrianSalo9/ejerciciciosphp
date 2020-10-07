1abb6e40d5fe0244b0de2cb8fdedc963e8a769d1
Adrian Salo <adriansalozgz@gmail.com>
2020-10-07 15:54:32 +0100

    ejercicio17

--- ejercicios/17/views/index.php
+++ ejercicios/17/views/index.php
@@ -1,8 +1,17 @@
-<header>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/ejercicios/17/css/css.css">
+  <title>Document</title>
+</head>
+<body>
   <ul>
     <li><a href="/ejercicios/17?method=fibonacci">Fibonacci</a></li>
     <li><a href="/ejercicios/17?method=listaPares">Pares</a></li>
     <li><a href="/ejercicios/17?method=potencias">Potencias</a></li>
     <li><a href="/ejercicios/17?method=primos">Primos</a></li>
   </ul>
-</header>
+</body>
+</html>
--- ejercicios/17/views/primos.php
+++ ejercicios/17/views/primos.php
@@ -3,16 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/ejercicios/17/css/css.css">
     <title>Document</title>
 </head>
 <body>
     <h1>Lista con los números primos</h1>
     <?php
-    foreach ($primos as $num) {
+    foreach ($primosbuenos as $num) {
         echo "Los primos son $num"." "."<br>";
     }
-    foreach ($noPrimos as $nume) {
-        echo "Los no primos son $nume"." "."<br>";
+    foreach ($primosmalos as $numm) {
+        echo "Los no primos son $numm"." "."<br>";
     }
     ?>
 </body>
