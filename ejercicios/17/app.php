81c7e99d110f11f574f52bfc9bf65a6de3cef663
Adrian Salo <adriansalozgz@gmail.com>
2020-10-07 16:35:28 +0100

    arreglos

--- ejercicios/17/app.php
+++ ejercicios/17/app.php
@@ -10,12 +10,15 @@ class app{
         $fibonacci=[];  
         $n1=1;
         $n2=0; 
-        for ($i=0; $i < 30; $i++) { 
-            $suma=$n1+$n2;
-            $n1=$n2;
-            $n2=$suma;
-            $fibonacci[]=$suma;
-        }
+        do {
+            for ($i=0; $i < 1; $i++) { 
+                $suma=$n1+$n2;
+                $n1=$n2;
+                $n2=$suma;
+                $fibonacci[]=$suma;
+            }
+        } while ($suma < 1000000);
+        
         include('views/fibonacci.php');
     }
     public function listaPares() 
