aa5ee01e9ef89123cac77af7bdd6401a564d20f7
Adrian Salo <adriansalozgz@gmail.com>
2020-10-21 16:14:06 +0100

    ejercicio20

---
+++ ejercicios/20/App.php
@@ -0,0 +1,52 @@
+<?php
+class App
+{
+    public function __construct()
+    {
+        session_start();
+    }
+    public function run()
+    {
+        if (isset($_GET['method'])) {
+            $method = $_GET['method'];
+        } else {
+            $method = 'index';
+        }
+
+        $this->$method();
+    }
+    public function login()
+    { {
+
+            if (isset($_SESSION['nombre'])) {
+                echo 'Ya hay un usuario';
+            } else {
+                $_SESSION['nombre'];
+                $_SESSION['contraseña'];
+                include('views/login.php');
+            }
+        }
+    }
+    public function auth()
+    {
+
+        $nombre = $_POST["nombre"];
+        $contraseña = $_POST["contraseña"];
+        $_SESSION['nombre']=$nombre;
+        $_SESSION['contraseña']=$contraseña;
+        header('Location: /ejercicios/20?method=home');
+    }
+    public function home()
+    {
+
+        if (isset($_SESSION)) {
+            echo 'Un saludo ' . $_SESSION['nombre'];
+        }
+        include('views/home.php');
+    }
+    public function logout()
+    {
+        session_destroy();
+        header('Location: /ejercicios/20?method=login');
+    }
+}
---
+++ ejercicios/20/views/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="?method=auth" method="POST">
+        <label>Nombre</label><input type="text" name="nombre" autocomplete="off" required><br>
+        <label>Contraseña</label><input type="password" name="contraseña" required><br>
+        <input type="submit" value="enviar">
+    </form>
+</body>
+
+</html>
